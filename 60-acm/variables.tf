1cecc0084ececbdb31dcc63b5209b1648dbf190a
narendranarra <narendralinux47@gmail.com>
2025-08-21 13:24:02 +0530

    jenkins

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z04446181QZWMSEOCZUNK"
+}
+
+variable "zone_name" {
+    default = "narendrablog.shop"
+}
